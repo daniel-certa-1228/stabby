152f3d477dce00326b24cfe1f259b65fd52a288c
Dan Certa <dcerta@oneeightycapital.com>
2024-04-12 15:22:58 -0500

    start initial sql

---
+++ scripts/Initial Insert SQL.sql
@@ -0,0 +1,59 @@
+-- UOM
+INSERT INTO stabby_web_unitofmeasure(uom_id, name)VALUES(1, 'in');
+INSERT INTO stabby_web_unitofmeasure(uom_id, name)VALUES(2, 'cm');
+
+-- COUNTRY
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(1, 'Austria', 'AUT');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(2, 'China', 'CHN');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(3, 'Czech Republic', 'CZE');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(4, 'England', 'GBR');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(5, 'France', 'FRA');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(6, 'Germany', 'DEU');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(7, 'Hong Kong', 'HKG');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(8, 'Italy', 'ITA');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(9, 'Japan', 'JPN');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(10, 'New Zealand', 'NZL');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(11, 'Portugal', 'PRT');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(12, 'Sweden', 'SWE');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(13, 'Switzerland', 'CHE');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(14, 'Taiwan', 'TWN');
+INSERT INTO stabby_web_country(country_id, name, code) VALUES(15, 'U.S.A.', 'USA');
+
+-- HANDLE MATERIAL
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(1, 'Aluminum');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(2,' Beech Wood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(3, 'Brass');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(4, 'Bronze');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(5, 'Burlwood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(6, 'Carbon Fiber');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(7, 'Cellidor');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(8, 'Cocobolo');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(9, 'Copper');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(10, 'Corelon');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(11, 'Delrin');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(12, 'Dymondwood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(13, 'Ebony');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(14, 'Fat Carbon');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(15, 'FRN');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(16, 'G-10');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(17, 'GFN');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(18, 'Ironwood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(19, 'Kirinite');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(20, 'Metal');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(21, 'Micarta');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(22, 'Mother of Pearl');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(23, 'Nylon');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(24, 'Oak');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(25, 'Olive Wood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(26, 'Rosewood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(27, 'Rubber');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(28, 'Shell (Metal)');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(29, 'Shell (Plastic over metal)');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(30, 'Stag');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(31, 'Steel');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(32, 'Stone');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(33, 'Synthetic');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(34, 'Titanium');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(35, 'Walnut');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(36, 'Wood');
+INSERT INTO stabby_web_handlematerial(handle_material_id, name) VALUES(37, 'Zytel');
