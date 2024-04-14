c3c4058fae043a6ba6fac483e969a3948cc25fb9
Dan Certa <dcerta@oneeightycapital.com>
2024-04-14 15:32:54 -0500

    setting up admin

--- stabby_web/admin.py
+++ stabby_web/admin.py
@@ -22,22 +22,97 @@ from .models import (
     WorkLog,
 )
 
-admin.site.register(BladeShape)
-admin.site.register(BondingAgent)
-admin.site.register(Country)
-admin.site.register(CuttingAgent)
-admin.site.register(DeploymentType)
-admin.site.register(HandleMaterial)
-admin.site.register(KnifeType)
-admin.site.register(LockType)
-admin.site.register(Lubricant)
-admin.site.register(SteelType)
-admin.site.register(UnitOfMeasure)
-admin.site.register(Vendor)
-admin.site.register(Brand)
-admin.site.register(SteelManufacturer)
-admin.site.register(BladeMaterial)
-admin.site.register(Knife)
-admin.site.register(Blade)
-admin.site.register(Sharpener)
-admin.site.register(WorkLog)
+
+@admin.register(BladeShape)
+class BladeShapeAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(BondingAgent)
+class BondingAgentAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(Country)
+class CountryAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(CuttingAgent)
+class CuttingAgentAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(DeploymentType)
+class DeploymentTypeAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(HandleMaterial)
+class HandleMaterialAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(KnifeType)
+class KnifeTypeAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(LockType)
+class LockTypeAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(Lubricant)
+class LubricantAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(SteelType)
+class SteelTypeAdmin(admin.ModelAdmin):
+    ordering = ["steel_type_id"]
+
+
+@admin.register(UnitOfMeasure)
+class UnitOfMeasureAdmin(admin.ModelAdmin):
+    ordering = ["uom_id"]
+
+
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(SteelManufacturer)
+class SteelManufacturerAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(BladeMaterial)
+class BladeMaterialAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(Knife)
+class KnifeAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(Blade)
+class KnifeAdmin(admin.ModelAdmin):
+    ordering = ["-is_main_blade", "knife_id"]
+
+
+@admin.register(Sharpener)
+class SharpenerAdmin(admin.ModelAdmin):
+    ordering = ["name"]
+
+
+@admin.register(WorkLog)
+class WorkLogAdmin(admin.ModelAdmin):
+    ordering = ["-date"]
