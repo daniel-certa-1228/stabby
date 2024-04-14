8c6926d135e0f79250b6e50ea451678b5a45bb5c
Dan Certa <dcerta@oneeightycapital.com>
2024-04-14 15:45:02 -0500

    more admin

--- stabby_web/admin.py
+++ stabby_web/admin.py
@@ -26,76 +26,90 @@ from .models import (
 @admin.register(BladeShape)
 class BladeShapeAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(BondingAgent)
 class BondingAgentAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(Country)
 class CountryAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(CuttingAgent)
 class CuttingAgentAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(DeploymentType)
 class DeploymentTypeAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(HandleMaterial)
 class HandleMaterialAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(KnifeType)
 class KnifeTypeAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(LockType)
 class LockTypeAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(Lubricant)
 class LubricantAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(SteelType)
 class SteelTypeAdmin(admin.ModelAdmin):
     ordering = ["steel_type_id"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(UnitOfMeasure)
 class UnitOfMeasureAdmin(admin.ModelAdmin):
     ordering = ["uom_id"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "is_active"]
 
 
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "parent_brand", "is_active"]
 
 
 @admin.register(SteelManufacturer)
 class SteelManufacturerAdmin(admin.ModelAdmin):
     ordering = ["name"]
+    list_display = ["name", "country", "is_active"]
 
 
 @admin.register(BladeMaterial)
 class BladeMaterialAdmin(admin.ModelAdmin):
-    ordering = ["name"]
+    list_display = ["name", "steel_type", "steel_manufacturer", "is_active"]
 
 
 @admin.register(Knife)
