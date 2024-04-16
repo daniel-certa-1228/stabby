49234b832c1a5119b27433777cca773fb3e81cf1
Dan Certa <dcerta@oneeightycapital.com>
2024-04-15 20:23:07 -0500

    bootstrap installed

--- stabby/settings.py
+++ stabby/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -112,11 +113,23 @@ USE_I18N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+# STATICFILES_DIRS = [BASE_DIR / "stabby_web" / "static"]
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "stabby_web/static"),
+]
+
+# Directory to export staticfiles for production
+# All files from all STATICFILES_DIRS will be copied by
+# manage.py collectstatic to this directory.
+# /!\ It will not be served by django, you have to setup
+# your webserver (or use a third party module)
+# to serve assets from there.
+STATIC_ROOT = BASE_DIR / "assets"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
