17aefe63af93e7c63aff89a852f7ede181c46ec8
Dan Certa <dcerta@oneeightycapital.com>
2024-04-18 22:21:10 -0500

    more grid stuff

--- stabby_web/static/stabby_web/js/load-knife-grid.js
+++ stabby_web/static/stabby_web/js/load-knife-grid.js
@@ -5,20 +5,21 @@ document.addEventListener('DOMContentLoaded', function() {
         const gridOptions = {
             pagination: true,
             defaultColDef: {
-                filter: true
+                filter: true,
+                cellStyle: {textAlign: 'left'}
               },
             columnDefs: [
-                { headerName: 'Id', field: 'knife_id', width: 70 },
-                { headerName: 'Knife', field: 'knife', width: 250 }, 
-                { headerName: 'Brand', field: 'brand' },
-                { headerName: '# of Blades', field: 'num_of_blades', width: 120 },
+                { headerName: 'Id', field: 'knife_id', width: 70, cellStyle: {textAlign: 'center'} },
+                { headerName: 'Knife', field: 'knife', width: 350 }, 
+                { headerName: 'Brand', field: 'brand', width: 220 },
+                { headerName: '# of Blades', field: 'num_of_blades', width: 120, cellStyle: {textAlign: 'right'} },
                 { headerName: 'Blade Material', field: 'blade_material', width: 250 },
                 { headerName: 'Handle Material', field: 'handle_material' },
                 { headerName: 'Lock', field: 'lock_type', width: 120 },
-                { headerName: 'Deployment', field: 'deployment_type' },
-                { headerName: 'Country', field: 'country', width: 120 },
-                { headerName: 'Vendor', field: 'vendor', width: 190 },
-                { headerName: 'Needs Work', field: 'needs_work', width: 140 },
+                { headerName: 'Deployment', field: 'deployment_type', width: 140 },
+                { headerName: 'Country', field: 'country', width: 140 },
+                { headerName: 'Vendor', field: 'vendor', width: 220 },
+                { headerName: 'Needs Work', field: 'needs_work', width: 140, cellStyle: {textAlign: 'center'} },
             ]
         };
 
