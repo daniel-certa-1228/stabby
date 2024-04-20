5d4a94b30461a85043fc54446106bbd52f2512aa
Dan Certa <dcerta@oneeightycapital.com>
2024-04-19 20:37:23 -0500

    basic sharpener grid working

--- stabby_web/static/stabby_web/js/load-knife-grid.js
+++ stabby_web/static/stabby_web/js/load-knife-grid.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const baseUrl = "http://127.0.0.1:8000/";
-
+    const baseUrl = "http://127.0.0.1:8000/api/";
+    // knife grid
     if (window.location.pathname === '/') {
         const gridDiv = document.querySelector('#grid');
 
@@ -28,9 +28,43 @@ document.addEventListener('DOMContentLoaded', function() {
         new agGrid.Grid(gridDiv, gridOptions);
 
         let xhr = new XMLHttpRequest();
-        xhr.open('GET', `${baseUrl}api/get_knife_grid`, true);
-        debugger;
+        xhr.open('GET', `${baseUrl}get_knife_grid`, true);
         xhr.onreadystatechange = function() {
+            if (xhr.readyState === 4 && xhr.status === 200) {
+                var responseData = JSON.parse(xhr.responseText);
+                gridOptions.api.setRowData(responseData);
+            }
+        };
+        xhr.send();
+    }
+    // sharpener grid
+    if (window.location.pathname === '/sharpeners/') {
+        const gridDiv = document.querySelector('#grid');
+
+        const gridOptions = {
+            defaultColDef: {
+                filter: true,
+                cellStyle: {textAlign: 'left'}
+              },
+            columnDefs: [
+                { headerName: 'Id', field: 'sharpener_id', width: 70, pinned: 'left', cellStyle: {textAlign: 'center'} },
+                { headerName: 'Brand', field: 'brand', width: 220, pinned: 'left' },
+                { headerName: 'Sharpener', field: 'sharpener', width: 350, pinned: 'left' }, 
+                { headerName: 'Cutting Agent', field: 'cutting_agent', width: 160 },
+                { headerName: 'Bonding Agent', field: 'bonding_agent', width: 190 },
+                { headerName: 'Length', field: 'length', width: 120 },
+                { headerName: 'Width', field: 'width', width: 120 },
+                { headerName: 'Country', field: 'country', width: 140 },
+                { headerName: 'Friable', field: 'is_friable', width: 140, cellStyle: {textAlign: 'center'} },
+            ]
+        };
+
+        new agGrid.Grid(gridDiv, gridOptions);
+
+        let xhr = new XMLHttpRequest();
+        xhr.open('GET', `${baseUrl}get_sharpener_grid`, true);
+        xhr.onreadystatechange = function() {
+            debugger;
             if (xhr.readyState === 4 && xhr.status === 200) {
                 var responseData = JSON.parse(xhr.responseText);
                 gridOptions.api.setRowData(responseData);
