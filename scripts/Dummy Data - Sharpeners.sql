2bf981f83c3825b0bb5ec306bb74ebbdc6a54366
Dan Certa <dcerta@oneeightycapital.com>
2024-04-19 19:03:25 -0500

    more dummy data

---
+++ scripts/Dummy Data - Sharpeners.sql
@@ -0,0 +1,12 @@
+INSERT INTO stabby_web_sharpener (name, notes, brand_id, brand_notes, country_id, cutting_agent_id, bonding_agent_id, lubricant_id, length, width, uom_id, is_active, create_date, edit_date, user_id) VALUES
+('Sharpener1', 'Lorem ipsum dolor sit amet', 36, 'Brand notes', 5, 2, 1, null, 3.5, 2.8, 1, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener2', 'Consectetur adipiscing elit', 62, 'Brand notes', 7, 1, 3, null, 4.2, 3.1, 2, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener3', 'Sed do eiusmod tempor', 15, 'Brand notes', 2, 3, 2, null, 3.9, 2.5, 1, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener4', 'Incididunt ut labore', 45, 'Brand notes', 8, 1, 1, null, 4.0, 3.0, 3, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener5', 'Et dolore magna aliqua', 20, 'Brand notes', 3, 2, 2, null, 3.7, 2.7, 2, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener6', 'Ut enim ad minim veniam', 69, 'Brand notes', 9, 1, 3, null, 4.1, 3.2, 1, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener7', 'Quis nostrud exercitation', 12, 'Brand notes', 4, 3, 1, null, 3.8, 2.9, 3, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener8', 'Ullamco laboris nisi ut aliquip', 55, 'Brand notes', 10, 2, 2, null, 4.3, 3.3, 2, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener9', 'Ex ea commodo consequat', 25, 'Brand notes', 6, 3, 3, null, 4.5, 3.4, 1, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener10', 'Duis aute irure dolor', 48, 'Brand notes', 1, 1, 1, null, 4.2, 3.1, 3, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2),
+('Sharpener11', 'In reprehenderit in voluptate', 18, 'Brand notes', 5, 2, 2, null, 4.4, 3.5, 2, 1, '2024-04-19 12:00:00', '2024-04-19 12:00:00', 2);
