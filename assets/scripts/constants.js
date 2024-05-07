5fe53d15b3bf93d6bbce2013a51857e0b5ab8edd
Dan Certa <dcerta@oneeightycapital.com>
2024-05-07 16:21:49 -0500

    added constants.js - not wired up yet

---
+++ assets/scripts/constants.js
@@ -0,0 +1,3 @@
+'use strict'
+
+export const baseUrl = "http://127.0.0.1:8000/";
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 import '@popperjs/core'
 import * as bootstrap from 'bootstrap';
+// import { baseUrl } from './constants'
 import './load-grids';
 import './toast-handler'
 import '../styles/styles.scss'
 
-export {bootstrap};
+export { bootstrap };
+// export { baseUrl }
