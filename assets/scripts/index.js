a71abb41aa09ddf3a8624311aab31e8bc2adfa13
Dan Certa <dcerta@oneeightycapital.com>
2024-05-07 22:05:49 -0500

    added imports but commented them out

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,12 +1,16 @@
 import '@popperjs/core';
 import * as bootstrap from 'bootstrap';
 // import { baseUrl } from './constants'
-import * as enums from './enums';
-import './page-handler';
+// import * as enums from './enums';
+// import * as ajax_handler from './ajax-handler';
+// import * as grid_loader from './grid_loader';
+// import './page-handler';
 import './load-grids';
 import './toast-handler';
 import '../styles/styles.scss';
 
 export { bootstrap };
+// export { ajax_handler };
+// export { grid_loader };
 // export { baseUrl }
-export { enums };
+// export { enums };
