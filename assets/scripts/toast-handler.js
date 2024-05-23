725750e7c262ec301fd54b7af6094d1209c6601d
Dan Certa <dcerta@oneeightycapital.com>
2024-05-22 21:06:16 -0500

    cleanup

--- assets/scripts/toast-handler.js
+++ assets/scripts/toast-handler.js
@@ -4,6 +4,7 @@ import { bootstrap } from './index';
 
 document.addEventListener('DOMContentLoaded', function() {
     const toasts = document.querySelectorAll('.toast');
+    
     toasts.forEach(function(toast) {
         const bootstrapToast = new bootstrap.Toast(toast);
         bootstrapToast.show();
