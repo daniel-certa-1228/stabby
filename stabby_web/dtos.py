14e5ec333ac865c936810802c5d792240c18725e
Dan Certa <dcerta@oneeightycapital.com>
2024-05-22 21:00:52 -0500

    bug fix

--- stabby_web/dtos.py
+++ stabby_web/dtos.py
@@ -1,10 +1,18 @@
 class TemplateVariableDTO:
-    def __init__(self, view_type, knife_id=None, sharpener_id=None, blade_id=None, work_log_id=None, photo_id=None):
+    def __init__(
+        self,
+        view_type,
+        knife_id=None,
+        sharpener_id=None,
+        blade_id=None,
+        work_log_id=None,
+        photo_id=None,
+    ):
         self.view_type = view_type
         self.knife_id = knife_id
         self.sharpener_id = sharpener_id
         self.blade_id = blade_id
-        self.work_log_id = work_log_id,
+        self.work_log_id = work_log_id
         self.photo_id = photo_id
 
     def to_dict(self):
@@ -14,5 +22,5 @@ class TemplateVariableDTO:
             "sharpener_id": self.sharpener_id,
             "blade_id": self.blade_id,
             "work_log_id": self.work_log_id,
-            "photo_id": self.photo_id
+            "photo_id": self.photo_id,
         }
