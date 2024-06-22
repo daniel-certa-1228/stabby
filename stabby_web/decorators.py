390056a9ab6eb993a439ca39e3a1a788bdaff8bf
Dan Certa <dcerta@oneeightycapital.com>
2024-06-22 16:53:14 -0500

    tweak

--- stabby_web/decorators.py
+++ stabby_web/decorators.py
@@ -4,12 +4,11 @@ from functools import wraps
 def skip_save(view_func):
     @wraps(view_func)
     def _wrapped_view(request, *args, **kwargs):
-        # Check if the logged-in user is in the "Collectors" group
         is_collector = False
+
         if request.user.is_authenticated:
             is_collector = request.user.groups.filter(name="Collectors").exists()
 
-        # Attach the `is_collector` flag to the request object
         request.is_collector = is_collector
 
         return view_func(request, *args, **kwargs)
