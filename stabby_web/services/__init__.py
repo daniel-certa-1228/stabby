a09d89d9c1058c6be052c434e274e0ef841d6593
Dan Certa <dcerta@oneeightycapital.com>
2024-07-18 16:27:15 -0500

    reorder

--- stabby_web/services/__init__.py
+++ stabby_web/services/__init__.py
@@ -2,16 +2,16 @@ from .blade_service import BladeService
 from .dashboard_service import DashboardService
 from .dropdown_service import DropdownService
 from .knife_service import KnifeService
+from .photo_service import PhotoService
 from .sharpener_service import SharpenerService
 from .work_log_service import WorkLogService
-from .photo_service import PhotoService
 
 __all__ = [
     "BladeService",
     "DashboardService",
     "DropdownService",
     "KnifeService",
+    "PhotoService",
     "SharpenerService",
     "WorkLogService",
-    "PhotoService",
 ]
