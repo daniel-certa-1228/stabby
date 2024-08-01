c66dbb8e3483733eea0de947eb56ce41f1b4998f
Dan Certa <dcerta@oneeightycapital.com>
2024-07-31 21:00:56 -0500

    new views

---
+++ sql_scripts/postgres/Blade Shape View Create - postgres.sql
@@ -0,0 +1,24 @@
+
+DROP VIEW IF EXISTS view_blade_shape_chart;
+
+CREATE VIEW view_blade_shape_chart AS
+	SELECT 
+		bs.Name,
+		COUNT(bs.blade_shape_id) AS "count",
+		ROUND((COUNT(bs.blade_shape_id) * 100.0 / (SELECT COUNT(*) FROM stabby_web_blade b2 LEFT JOIN stabby_web_knife k2 ON k2.knife_id = b2.knife_id AND k2.is_active = TRUE WHERE b2.is_active = true)), 2) AS percentage
+	FROM 
+		stabby_web_blade b
+	LEFT JOIN 
+		stabby_web_knife k ON k.knife_id = b.knife_id 
+	LEFT JOIN 
+		stabby_web_bladeshape bs ON bs.blade_shape_id = b.blade_shape_id 
+	WHERE 
+		b.is_active = TRUE 
+		AND k.is_active = TRUE
+	GROUP BY 
+		bs.Name
+	ORDER BY 
+		"count" DESC, 
+		bs.name;
+		
+GRANT SELECT ON view_blade_shape_chart TO db;
---
+++ sql_scripts/postgres/USA New-Vintage View Create - postgres.sql
@@ -0,0 +1,31 @@
+
+DROP VIEW IF EXISTS view_usa_new_vintage_chart;
+
+CREATE VIEW view_usa_new_vintage_chart AS
+	SELECT 
+	"name",
+	"count",
+	ROUND(("count" * 100.0 / (SELECT COUNT(*) FROM stabby_web_knife WHERE is_active = TRUE AND country_id = 16)), 2) AS percentage
+FROM (
+	SELECT 
+		'New' AS "name",
+		count(*) 
+	FROM stabby_web_knife k
+	WHERE 
+		k.is_active = TRUE
+		AND k.country_id = 16
+		AND k.purchased_new = TRUE
+	UNION ALL 
+	SELECT 
+		'Vintage' AS "name",
+		count(*) 
+	FROM stabby_web_knife k
+	WHERE 
+		k.is_active = TRUE
+		AND k.country_id = 16
+		AND k.purchased_new = False
+) a
+ORDER BY
+	"count" DESC;
+		
+GRANT SELECT ON view_usa_new_vintage_chart TO db;
