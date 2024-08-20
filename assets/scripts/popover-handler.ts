1be3666af3051772e641c2ac678cd31e38e5e499
Dan Certa <dcerta@oneeightycapital.com>
2024-08-19 19:19:43 -0500

    popover handlers working

--- assets/scripts/popover-handler.ts
+++ assets/scripts/popover-handler.ts
@@ -2,9 +2,21 @@
 
 import { bootstrap } from './index';
 
+// SERVER-RENDERED POPOVERS
 document.addEventListener('DOMContentLoaded', function() {
   const popoverTriggerList: NodeListOf<Element> = document.querySelectorAll('[data-bs-toggle="popover"]');
   
     // eslint-disable-next-line no-unused-vars
   const popoverList: bootstrap.Popover[] = Array.from(popoverTriggerList).map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 });
+
+//GRID POPOVERS
+const initGridPopovers = (): void => {
+  const popovers: HTMLCollectionOf<Element> = document.getElementsByClassName('grid-popover');
+
+  const popoverList: bootstrap.Popover[] = Array.from(popovers).map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
+}
+
+export {
+  initGridPopovers
+}
