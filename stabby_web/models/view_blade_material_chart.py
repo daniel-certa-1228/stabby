b1986cb73753dcff1a496e1d7e09a59566ec82cb
Dan Certa <dcerta@oneeightycapital.com>
2024-08-30 18:58:25 -0500

    added missing field

--- stabby_web/models/view_blade_material_chart.py
+++ stabby_web/models/view_blade_material_chart.py
@@ -7,6 +7,7 @@ class ViewBladeMaterialChart(models.Model):
     percentage = models.DecimalField(
         null=True, blank=True, decimal_places=2, max_digits=8
     )
+    query_str = models.CharField(max_length=100, null=True, blank=True)
 
     class Meta:
         db_table = "view_blade_material_chart"
