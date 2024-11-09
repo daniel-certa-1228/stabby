0d55d0bf821adc79a27ffe4e62776fdf18821e9e
Dan Certa <dcerta@oneeightycapital.com>
2024-11-09 10:08:20 -0600

    Changed model to selectively shrink photos

--- stabby_web/models/photo_model.py
+++ stabby_web/models/photo_model.py
@@ -1,15 +1,15 @@
+from io import BytesIO
+from PIL import Image
 from django.db import models
 from .brand_model import Brand
 from .knife_model import Knife
 from .sharpener_model import Sharpener
-from django_resized import ResizedImageField
+from django.core.files.base import ContentFile
 
 
 class Photo(models.Model):
     photo_id = models.AutoField(primary_key=True, db_column="photo_id")
-    photo = ResizedImageField(
-        size=[1200, 900], upload_to="images/", quality=85, force_format="JPEG"
-    )
+    photo = models.ImageField(upload_to="images/")
     description = models.TextField(null=True, blank=True)
     knife = models.ForeignKey(
         Knife, related_name="photos", on_delete=models.SET_NULL, null=True, blank=True
@@ -36,6 +36,24 @@ class Photo(models.Model):
         verbose_name = "photo"
         verbose_name_plural = "photos"
         ordering = ["create_date"]
+
+    def save(self, *args, **kwargs):
+        # Open the image file
+        img = Image.open(self.photo)
+
+        # Check the resolution of the image
+        if img.width > 1200 or img.height > 900:
+            # Resize the image only if it's larger than the desired size
+            img = img.resize((1200, 900), Image.LANCZOS)
+            output = BytesIO()
+            img.save(output, format="JPEG", quality=85)
+            output.seek(0)
+
+            # Replace the old photo with the resized one
+            self.photo = ContentFile(output.read(), self.photo.name)
+
+        # Call the parent class's save method to save the model
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.description if self.description else f"Photo {self.photo_id}"
--- stabby_web/services/photo_service.py
+++ stabby_web/services/photo_service.py
@@ -1,5 +1,4 @@
 from django.shortcuts import get_object_or_404
-from django.utils import timezone
 from ..models import Photo
 
 
