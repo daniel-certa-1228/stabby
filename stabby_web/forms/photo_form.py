6d61276f386a82b659b828eaaf4f0ed633cd9fe4
Dan Certa <dcerta@oneeightycapital.com>
2024-11-20 19:05:32 -0600

    restore clean to foto form

--- stabby_web/forms/photo_form.py
+++ stabby_web/forms/photo_form.py
@@ -17,18 +17,24 @@ class PhotoForm(forms.ModelForm):
         active = kwargs.pop("active", None)  # Extract 'active' from kwargs
         super().__init__(*args, **kwargs)
 
-        # Conditionally set 'brand' as required if 'active' is 'library'
-        if active == "library":
-            self.fields["brand"].required = True
-        else:
-            self.fields["brand"].required = False
-
         # Bootstrap styling and other initializations
         for field_name, field in self.fields.items():
             field.widget.attrs["class"] = "form-control form-control-sm mb-2"
             if isinstance(field.widget, forms.Textarea):
                 field.widget.attrs["rows"] = 4
 
+        # Conditionally set 'brand' as required if 'active' is 'library'
+        if active == "library":
+            self.fields["brand"].required = True
+        else:
+            self.fields["brand"].required = False
+
         # Dropdown queryset for brand
         self.fields["brand"].queryset = DropdownService.get_brands()
         self.fields["brand"].empty_label = "Select Brand"
+
+    def clean_description(self):
+        description = self.cleaned_data.get("description")
+        if description:
+            return description.strip()
+        return description
